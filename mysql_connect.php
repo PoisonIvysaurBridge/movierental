66858d882c7091267c41c6978b349adf32cf263d
PoisonIvysaurBridge <ivana_lim@dlsu.edu.ph>
2017-08-04 00:14:23 +0800

    Update mysql_connect.php

--- mysql_connect.php
+++ mysql_connect.php
@@ -1,5 +1,5 @@
 <?php
-    $dbc=mysqli_connect('127.0.0.1','root','l;lkl k ','movierentaldb');
+    $dbc=mysqli_connect('127.0.0.1','root','','movierentaldb');
 
     if (!$dbc) {
     die('Could not connect: '.mysql_error());
