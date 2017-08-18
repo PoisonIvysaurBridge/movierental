ff4abbad4435ece8f15be8f3073ea40697124d8b
Ivy <ivana_lim@dlsu.edu.ph>
2017-08-18 15:00:02 +0800

    polished address file

--- address.php
+++ address.php
@@ -46,6 +46,7 @@
     <div class="w3-container"  style="margin: 0 30px;" >
         <h1 style="text-align: center;">New Customer Registration</h1>
         <div class="w3-container" style="text-align:center;">
+        <div id="wrapper">
         <div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-12">
@@ -98,7 +99,7 @@
         </div>
         <!-- /#page-wrapper -->
         </div>
-    
+        </div>
         <div style="float:left; width: 60%;">
         <form action="#" method="post">
             <input type="hidden" name="firstname" value="<?php echo $_POST['firstname']; ?>">
@@ -319,6 +320,29 @@
         ?>
 
     </div>
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="vendor/metisMenu/metisMenu.min.js"></script>
+
+    <!-- DataTables JavaScript -->
+    <script src="vendor/datatables/js/jquery.dataTables.min.js"></script>
+    <script src="vendor/datatables-plugins/dataTables.bootstrap.min.js"></script>
+    <script src="vendor/datatables-responsive/dataTables.responsive.js"></script>
+
+
+    <!-- Page-Level Demo Scripts - Tables - Use for reference -->
+    <script>
+    $(document).ready(function() {
+        $('#dataTables-example').DataTable({
+            responsive: true
+        });
+    });
+    </script>
     <script>
     function dynamicSelect(ajaxPage, id){
     $.ajax({
